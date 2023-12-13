af3ce8bdd0a5a714b4dffb98e6af08035f9de2f9
BoySMOOTH <adekunleodusola@gmail.com>
2023-12-13 14:39:25 -0800

    _signal.c

---
+++ _signal.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * _signal - it keeps track in interactive mode
+ * @m: the signal number
+ * Return: nothing
+ */
+
+void _signal(int m)
+{
+	(void)m;
+	write(STDERR_FILENO, "\n", 1);
+	write(STDERR_FILENO, "$ ", 2);
+}
