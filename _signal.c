ffad4ff62ad7314261c89f0063564575135e3725
BoySMOOTH <adekunleodusola@gmail.com>
2023-12-14 23:49:20 -0800

    _signal.c

--- _signal.c
+++ _signal.c
@@ -1,4 +1,4 @@
-#include "shell.h"
+#include "main.h"
 
 /**
  * _signal - it keeps track in interactive mode
--- betty_task.c
+++ betty_task.c
@@ -1,7 +1,7 @@
 #include "main.h"
 
 /**
- * add - adds 2 variables
+ * add - adds two variables
  * @no1: the first number to be added
  * @no2: the second number to be added
  * Return: The added values
